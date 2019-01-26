0ddaff990f55390764e80f993f39e7eff8ce7302
GurwinderSethi <gurwinder95@gmail.com>
2019-01-26 15:38:40 +0530

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 //mongoose.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser:true});
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -32,6 +33,22 @@ app.get('/todos',(req,res)=>{
 },(e)=>{
 	res.status(400).send(e);
 });
+});
+
+app.get('/todos/:id',(req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();	
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+	//res.send(req.params);
 });
 
 app.listen(3000,()=>{
