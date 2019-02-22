a3755f5338fd13be5797f99f6ee7683b64d920ed
GurwinderSethi <gurwinder95@gmail.com>
2019-02-22 16:59:05 +0530

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 //mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser:true});
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -11,6 +12,9 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+
+const port = process.env.PORT || 3000 ;
+
 app.use(bodyParser.json());	
 
 app.post('/todos',(req,res)=>{
@@ -51,8 +55,23 @@ app.get('/todos/:id',(req,res) => {
 	//res.send(req.params);
 });
 
-app.listen(3000,()=>{
-	console.log('Started on port 3000');
+//POST/users
+app.post('/users',(req,res)=>{
+	var body = _.pick(req.body,['email','password']);
+	var user = new User(body);
+	
+	user.save().then(()=>{
+		return user.generateAuthToken();
+		//res.send(user);
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((e)=>{
+		res.status(400).send(e);
+	})
+});
+
+app.listen(port,()=>{
+	console.log('Started up at port:'+port);
 });
 
 /*
